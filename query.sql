5965442db5f37bd5f683e0e51be738f81dfa8699
KuR0zZ <ferdinandiphone6@gmail.com>
2025-02-06 16:24:37 +0700

    refactor: modify table fields and add trigger to update updated_at

--- query.sql
+++ query.sql
@@ -5,6 +5,21 @@ CREATE TABLE Notifications (
 	email VARCHAR NOT NULL,
 	message TEXT NOT NULL,
 	type VARCHAR NOT NULL,
-	status VARCHAR NOT NULL,
+	is_sent BOOLEAN NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER update_notification_modtime
+BEFORE UPDATE ON Notifications
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
